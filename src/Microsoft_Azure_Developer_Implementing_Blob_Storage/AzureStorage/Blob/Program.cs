de8f966b1d452a3d05296a285c7d9b261b0fdd2a
vsha041 <varun.varunsharma@gmail.com>
2019-09-29 21:10:48 +1300

    Added code to upload blob.

---
+++ src/Microsoft_Azure_Developer_Implementing_Blob_Storage/AzureStorage/Blob/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using Blob.Services;
+using Blob.Settings;
+
+namespace Blob
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Upload Blob
+			var image = Image.FromFile(@"Images\ben.jpg");
+			var uploadBlobService = new UploadBlobService(AppSettings.ConnectionString);
+			var task = Task.Run(async () => await uploadBlobService.Upload(image, "ben.jpg"));
+			task.Wait();
+			Console.WriteLine();
+		}
+	}
+}
---
+++ src/Microsoft_Azure_Developer_Implementing_Blob_Storage/AzureStorage/Blob/Services/UploadBlobService.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+
+namespace Blob.Services
+{
+	public class UploadBlobService
+	{
+		private readonly string _containerName = "images";
+		private readonly string _connectionString;
+
+		public UploadBlobService(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public async Task Upload(Image image, string blobName)
+		{
+			var cloudStorageAccount = CloudStorageAccount.Parse(_connectionString);
+			var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+			var cloudBlobContainer = cloudBlobClient.GetContainerReference(_containerName);
+			await cloudBlobContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
+			var blockBlobReference = cloudBlobContainer.GetBlockBlobReference(blobName);
+			var imageByteArray = GetImageBytes(image);
+			await blockBlobReference.UploadFromByteArrayAsync(imageByteArray, 0, imageByteArray.Length);
+		}
+
+		private byte[] GetImageBytes(Image image)
+		{
+			using var ms = new MemoryStream();
+			image.Save(ms, image.RawFormat);
+			return ms.ToArray();
+		}
+	}
+}
