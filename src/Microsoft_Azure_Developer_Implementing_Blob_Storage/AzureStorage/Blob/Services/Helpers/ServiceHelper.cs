96850716b2994657d92176a9ebfaf95ff44d5fca
vsha041 <varun.varunsharma@gmail.com>
2019-10-09 19:48:47 +1300

    Refactored the code

---
+++ src/Microsoft_Azure_Developer_Implementing_Blob_Storage/AzureStorage/Blob/Services/Helpers/ServiceHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+
+namespace Blob.Services.Helpers
+{
+	public class ServiceHelper
+	{
+		private readonly string _containerName;
+		private readonly string _connectionString;
+
+		public ServiceHelper(string connectionString, string containerName)
+		{
+			_connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+			_containerName = containerName ?? throw new ArgumentNullException(nameof(containerName));
+		}
+
+		public async Task<CloudBlobContainer> GetCloudBlobContainer()
+		{
+			var cloudStorageAccount = CloudStorageAccount.Parse(_connectionString);
+			var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+			var cloudBlobContainer = cloudBlobClient.GetContainerReference(_containerName);
+			await cloudBlobContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
+			return cloudBlobContainer;
+		}
+
+		public byte[] GetImageBytes(Image image)
+		{
+			using var ms = new MemoryStream();
+			image.Save(ms, image.RawFormat);
+			return ms.ToArray();
+		}
+	}
+}
--- src/Microsoft_Azure_Developer_Implementing_Blob_Storage/AzureStorage/Blob/Services/UploadBlobService.cs
+++ src/Microsoft_Azure_Developer_Implementing_Blob_Storage/AzureStorage/Blob/Services/UploadBlobService.cs
@@ -1,50 +1,35 @@
 ﻿using System.Drawing;
-using System.IO;
 using System.Threading.Tasks;
 using Blob.Interfaces;
-using Microsoft.Azure.Storage;
+using Blob.Services.Helpers;
 using Microsoft.Azure.Storage.Blob;
 
 namespace Blob.Services
 {
 	public class UploadBlobService : IStorage
 	{
-		private readonly string _containerName = "images";
-		private readonly string _connectionString;
+		private const string ContainerName = "images";
+		private readonly ServiceHelper _serviceHelper;
 
-		public UploadBlobService(string connectionString)
+		public UploadBlobService (string connectionString)
 		{
-			_connectionString = connectionString;
+			_serviceHelper = new ServiceHelper(connectionString, ContainerName);
 		}
 
 		public async Task<CloudBlockBlob> Upload(Image image, string blobName)
 		{
-			CloudBlobContainer cloudBlobContainer = await GetCloudBlobContainer();
+			CloudBlobContainer cloudBlobContainer = await _serviceHelper.GetCloudBlobContainer();
 			CloudBlockBlob blockBlobReference = cloudBlobContainer.GetBlockBlobReference(blobName);
-			var imageByteArray = GetImageBytes(image);
+			var imageByteArray = _serviceHelper.GetImageBytes(image);
 			await blockBlobReference.UploadFromByteArrayAsync(imageByteArray, 0, imageByteArray.Length);
 			return blockBlobReference;
 		}
 
-		private async Task<CloudBlobContainer> GetCloudBlobContainer()
-		{
-			var cloudStorageAccount = CloudStorageAccount.Parse(_connectionString);
-			var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
-			var cloudBlobContainer = cloudBlobClient.GetContainerReference(_containerName);
-			await cloudBlobContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
-			return cloudBlobContainer;
-		}
-
-		private byte[] GetImageBytes(Image image)
-		{
-			using var ms = new MemoryStream();
-			image.Save(ms, image.RawFormat);
-			return ms.ToArray();
-		}
+		
 
 		public async Task<bool> CheckIfBlobExistsAsync(string blobName)
 		{
-			CloudBlobContainer cloudBlobContainer = await GetCloudBlobContainer();
+			CloudBlobContainer cloudBlobContainer = await _serviceHelper.GetCloudBlobContainer();
 			CloudBlockBlob blockBlobReference = cloudBlobContainer.GetBlockBlobReference(blobName);
 			return await blockBlobReference.ExistsAsync();
 		}
